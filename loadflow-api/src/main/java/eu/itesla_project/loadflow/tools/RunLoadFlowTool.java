12a4c260e548923088817f3ba7702978ad872577
Christian Biasuzzi <christian.biasuzzi@quinary.com>
2017-05-23 13:15:14 +0200

    adds LoadFlowResult de/serializer; adds json option to run-loadflow itool

--- loadflow-api/src/main/java/eu/itesla_project/loadflow/tools/RunLoadFlowTool.java
+++ loadflow-api/src/main/java/eu/itesla_project/loadflow/tools/RunLoadFlowTool.java
@@ -4,17 +4,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-
 package eu.itesla_project.loadflow.tools;
 
 import com.google.auto.service.AutoService;
 import eu.itesla_project.commons.config.ComponentDefaultConfig;
-import eu.itesla_project.commons.io.table.Column;
-import eu.itesla_project.commons.io.table.TableFormatter;
-import eu.itesla_project.commons.io.table.TableFormatterConfig;
-import eu.itesla_project.commons.io.table.TableFormatterUtils;
+import eu.itesla_project.commons.io.table.*;
 import eu.itesla_project.commons.tools.Command;
 import eu.itesla_project.commons.tools.Tool;
+import eu.itesla_project.commons.tools.ToolRunningContext;
 import eu.itesla_project.computation.ComputationManager;
 import eu.itesla_project.computation.local.LocalComputationManager;
 import eu.itesla_project.iidm.import_.ImportConfig;
@@ -23,14 +20,19 @@ import eu.itesla_project.iidm.network.Network;
 import eu.itesla_project.loadflow.api.LoadFlow;
 import eu.itesla_project.loadflow.api.LoadFlowFactory;
 import eu.itesla_project.loadflow.api.LoadFlowResult;
+import eu.itesla_project.loadflow.json.LoadFlowResultSerializer;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.Arrays;
 
 /**
  * @author Christian Biasuzzi <christian.biasuzzi@techrain.it>
@@ -38,110 +40,176 @@ import java.nio.file.Paths;
 @AutoService(Tool.class)
 public class RunLoadFlowTool implements Tool {
 
-    private static final String TABLE_TITLE = "loadflow";
-    private static Command COMMAND = new Command() {
-
-        @Override
-        public String getName() {
-            return "run-loadflow";
-        }
-
-        @Override
-        public String getTheme() {
-            return "Computation";
-        }
-
-        @Override
-        public String getDescription() {
-            return "Run loadflow";
-        }
-
-        @Override
-        public Options getOptions() {
-            Options options = new Options();
-            options.addOption(Option.builder().longOpt("case-file")
-                    .desc("the case path")
-                    .hasArg()
-                    .argName("FILE")
-                    .required()
-                    .build());
-            options.addOption(Option.builder().longOpt("skip-postproc")
-                    .desc("skip network importer post processors (when configured)")
-                    .build());
-            options.addOption(Option.builder().longOpt("output-file")
-                    .desc("export to a file")
-                    .hasArg()
-                    .argName("FILE")
-                    .build());
-            options.addOption(Option.builder().longOpt("output-format")
-                    .desc("output formats available: " + TableFormatterUtils.availableTableFormatterFormats() + " (default is ascii)")
-                    .hasArg()
-                    .argName("FORMAT")
-                    .build());
-            return options;
-        }
-
-        @Override
-        public String getUsageFooter() {
-            return null;
-        }
-
-    };
+    private enum Format {
+        CSV,
+        JSON
+    }
 
     @Override
     public Command getCommand() {
-        return COMMAND;
-    }
+        return new Command() {
+            @Override
+            public String getName() {
+                return "run-loadflow";
+            }
 
-    private TableFormatter getFormatter(String outputFormat, Path outputFile) throws IOException {
-        TableFormatterConfig tableFormatterConfig = TableFormatterConfig.load();
-        Column[] tableColumns = {
-                new Column("Network"),
-                new Column("Result"),
-                new Column("Metrics"),
+            @Override
+            public String getTheme() {
+                return "Computation";
+            }
+
+            @Override
+            public String getDescription() {
+                return "Run loadflow";
+            }
+
+            @Override
+            public Options getOptions() {
+                Options options = new Options();
+                options.addOption(Option.builder().longOpt("case-file")
+                        .desc("the case path")
+                        .hasArg()
+                        .argName("FILE")
+                        .required()
+                        .build());
+                options.addOption(Option.builder().longOpt("output-file")
+                        .desc("the output path")
+                        .hasArg()
+                        .argName("FILE")
+                        .build());
+                options.addOption(Option.builder().longOpt("output-format")
+                        .desc("the output format " + Arrays.toString(Format.values()))
+                        .hasArg()
+                        .argName("FORMAT")
+                        .build());
+                options.addOption(Option.builder().longOpt("skip-postproc")
+                        .desc("skip network importer post processors (when configured)")
+                        .build());
+                return options;
+            }
+
+            @Override
+            public String getUsageFooter() {
+                return null;
+            }
         };
-        return TableFormatterUtils.createFormatter(tableFormatterConfig, outputFormat, outputFile, TABLE_TITLE, tableColumns);
-
-    }
-
-    private void printTableEntry(TableFormatter formatter, Network network, LoadFlowResult result) {
-        try {
-            formatter.writeCell(network.getId());
-            formatter.writeCell(result.isOk() ? "ok" : "nok");
-            formatter.writeCell(result.getMetrics().toString());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
     }
 
     @Override
-    public void run(CommandLine line) throws Exception {
-        Path caseFile = Paths.get(line.getOptionValue("case-file"));
-        Path outputFile = (line.hasOption("output-file")) ? Paths.get(line.getOptionValue("output-file")) : null;
-        String outputFormat = (line.hasOption("output-format")) ? line.getOptionValue("output-format") : "ascii";
+    public void run(CommandLine line, ToolRunningContext context) throws Exception {
+        Path caseFile = context.getFileSystem().getPath(line.getOptionValue("case-file"));
         boolean skipPostProc = line.hasOption("skip-postproc");
+        Path outputFile = null;
+        Format format = null;
         ComponentDefaultConfig defaultConfig = ComponentDefaultConfig.load();
 
-        try (ComputationManager computationManager = new LocalComputationManager(); TableFormatter formatter = getFormatter(outputFormat, outputFile)) {
-            ImportConfig importConfig = (skipPostProc == false) ? ImportConfig.load() : new ImportConfig();
+        try (ComputationManager computationManager = new LocalComputationManager()) {
+            ImportConfig importConfig = (!skipPostProc) ? ImportConfig.load() : new ImportConfig();
             if (Files.isRegularFile(caseFile)) {
+                // process a single network: output-file/output-format options available
+                if (line.hasOption("output-file")) {
+                    outputFile = context.getFileSystem().getPath(line.getOptionValue("output-file"));
+                    if (!line.hasOption("output-format")) {
+                        throw new ParseException("Missing required option: output-format");
+                    }
+                    format = Format.valueOf(line.getOptionValue("output-format"));
+                }
+
+                context.getOutputStream().println("Loading network '" + caseFile + "'");
                 Network network = Importers.loadNetwork(caseFile, computationManager, importConfig, null);
                 if (network == null) {
                     throw new RuntimeException("Case '" + caseFile + "' not found");
                 }
                 LoadFlow loadFlow = defaultConfig.newFactoryImpl(LoadFlowFactory.class).create(network, computationManager, 0);
-                printTableEntry(formatter, network, loadFlow.run());
+                LoadFlowResult result = loadFlow.run();
+
+                if (outputFile != null) {
+                    exportResult(result, context, outputFile, format);
+                } else {
+                    printResult(result, context);
+                }
+
             } else if (Files.isDirectory(caseFile)) {
-                Importers.loadNetworks(caseFile, false, computationManager, importConfig, network -> {
-                    try {
-                        LoadFlow loadFlow = defaultConfig.newFactoryImpl(LoadFlowFactory.class).create(network, computationManager, 0);
-                        printTableEntry(formatter, network, loadFlow.run());
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }, dataSource -> System.out.println("loading case " + dataSource.getBaseName()));
+                //process multiple networks: concise output (results table, to the standard output)
+                try (TableFormatter formatter = getAsciiFormatter(TableFormatterConfig.load(), context)) {
+                    Importers.loadNetworks(caseFile, false, computationManager, importConfig, network -> {
+                        try {
+                            LoadFlow loadFlow = defaultConfig.newFactoryImpl(LoadFlowFactory.class).create(network, computationManager, 0);
+                            LoadFlowResult result = loadFlow.run();
+                            printTableEntry(formatter, network, result);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }, dataSource -> context.getOutputStream().println("Loading case '" + dataSource.getBaseName() + "'"));
+                }
             }
+        }
+    }
+
+
+    private TableFormatter getAsciiFormatter(TableFormatterConfig formatterConfig, ToolRunningContext context) {
+        Writer writer = new OutputStreamWriter(context.getOutputStream()) {
+            @Override
+            public void close() throws IOException {
+                flush();
+            }
+        };
+        AsciiTableFormatterFactory asciiTableFormatterFactory = new AsciiTableFormatterFactory();
+        return asciiTableFormatterFactory.create(writer,
+                "loadflow results",
+                formatterConfig,
+                new Column("Network"),
+                new Column("Result"),
+                new Column("Metrics"));
+    }
+
+    private void printTableEntry(TableFormatter formatter, Network network, LoadFlowResult result) {
+        try {
+            formatter.writeCell(network.getId());
+            formatter.writeCell(result.isOk());
+            formatter.writeCell(result.getMetrics().toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void printLoadFlowResult(LoadFlowResult result, Writer writer, TableFormatterFactory formatterFactory,
+                                     TableFormatterConfig formatterConfig) {
+        try (TableFormatter formatter = formatterFactory.create(writer,
+                "loadflow results",
+                formatterConfig,
+                new Column("Result"),
+                new Column("Metrics"))) {
+            formatter.writeCell(result.isOk());
+            formatter.writeCell(result.getMetrics().toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void printResult(LoadFlowResult result, ToolRunningContext context) {
+        Writer writer = new OutputStreamWriter(context.getOutputStream()) {
+            @Override
+            public void close() throws IOException {
+                flush();
+            }
+        };
+        AsciiTableFormatterFactory asciiTableFormatterFactory = new AsciiTableFormatterFactory();
+        printLoadFlowResult(result, writer, asciiTableFormatterFactory, TableFormatterConfig.load());
+    }
+
+
+    private void exportResult(LoadFlowResult result, ToolRunningContext context, Path outputFile, Format format) throws IOException {
+        context.getOutputStream().println("Writing results to '" + outputFile + "'");
+        switch (format) {
+            case CSV:
+                CsvTableFormatterFactory csvTableFormatterFactory = new CsvTableFormatterFactory();
+                printLoadFlowResult(result, Files.newBufferedWriter(outputFile, StandardCharsets.UTF_8), csvTableFormatterFactory, TableFormatterConfig.load());
+                break;
+
+            case JSON:
+                LoadFlowResultSerializer.write(result, outputFile);
+                break;
         }
     }
 }
