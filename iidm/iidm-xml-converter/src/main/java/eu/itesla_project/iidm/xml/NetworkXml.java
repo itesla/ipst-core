a52469f6ab407bfe3a0f7c9de2d97e8a669fd319
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2017-08-27 23:01:14 +0200

    Wrap JDK checked exceptions to unchecked ones

--- iidm/iidm-xml-converter/src/main/java/eu/itesla_project/iidm/xml/NetworkXml.java
+++ iidm/iidm-xml-converter/src/main/java/eu/itesla_project/iidm/xml/NetworkXml.java
@@ -8,6 +8,8 @@ package eu.itesla_project.iidm.xml;
 
 import com.google.common.base.Supplier;
 import com.google.common.base.Suppliers;
+import eu.itesla_project.commons.exceptions.UncheckedSaxException;
+import eu.itesla_project.commons.exceptions.UncheckedXmlStreamException;
 import eu.itesla_project.commons.util.ServiceLoaderCache;
 import eu.itesla_project.iidm.network.*;
 import javanet.staxutils.IndentingXMLStreamWriter;
@@ -96,8 +98,10 @@ public class NetworkXml implements XmlConstants {
             Schema schema = factory.newSchema(sources);
             Validator validator = schema.newValidator();
             validator.validate(xml);
-        } catch (SAXException | IOException e) {
-            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        } catch (SAXException e) {
+            throw new UncheckedSaxException(e);
         }
     }
 
@@ -219,7 +223,7 @@ public class NetworkXml implements XmlConstants {
 
             return anonymizer;
         } catch (XMLStreamException e) {
-            throw new RuntimeException(e);
+            throw new UncheckedXmlStreamException(e);
         }
     }
 
@@ -332,7 +336,7 @@ public class NetworkXml implements XmlConstants {
 
             return network;
         } catch (XMLStreamException e) {
-            throw new RuntimeException(e);
+            throw new UncheckedXmlStreamException(e);
         }
     }
 
@@ -416,7 +420,7 @@ public class NetworkXml implements XmlConstants {
                 }
             });
         } catch (XMLStreamException e) {
-            throw new RuntimeException(e);
+            throw new UncheckedXmlStreamException(e);
         }
     }
 
