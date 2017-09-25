370da0ac67433cfb986613636c7aa57e517aa171
yichen88 <tang.yichenyves@gmail.com>
2017-09-25 09:54:07 +0200

    Add ast transform for custom methods

---
+++ action/action-simulator/src/test/java/eu/itesla_project/action/simulator/PreDefinedMethodsTest.java
@@ -0,0 +1,31 @@
+package eu.itesla_project.action.simulator;
+
+import eu.itesla_project.iidm.network.Network;
+import org.junit.Test;
+
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+
+public class PreDefinedMethodsTest extends AbstractLoadFlowRulesEngineTest {
+    @Override
+    protected Network createNetwork() {
+        Network network = EurostagTutorialExample1WithTemporaryLimitFactory.create();
+
+        // overload the line
+        network.getVoltageLevel("VLHV1").getBusBreakerView().getBus("NHV1").setV(380).setAngle(0);
+        network.getLine("NHV1_NHV2_2").getTerminal1().setP(300).setQ(100);
+
+        return network;
+    }
+
+    @Override
+    protected String getDslFile() {
+        return "/transform_in_methods.groovy";
+    }
+
+    @Test
+    public void test() throws Exception {
+        engine.start(actionDb);
+    }
+}
